cd89d8031706f07dc0f10574372b34e76a298f85
Léon Brodbeck <brodbeckleon@gmail.com>
2025-05-21 19:41:00 +0200

    restructuring

---
+++ frontend/src/lib/components/common/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+
+	interface ButtonProps {
+		label: string;
+		title?: string;
+		icon?: SvelteComponent; // Changed type to accept a Svelte component
+		onClick?: () => void;
+		href?: string;
+		disabled?: boolean;
+	}
+	const { label, title, icon, onClick, disabled }: ButtonProps = $props();
+</script>
+
+<button
+	type="button"
+	class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-gray-500/10 ring-inset"
+	{title}
+	onclick={onClick}
+	{disabled}
+>
+	{label}
+	{#if icon}
+		<span class="ml-1">
+			<icon> </icon>
+		</span>
+	{/if}
+</button>
---
+++ frontend/src/lib/components/navigation/NavigationTopBar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface NavigationProps {
+		href: string;
+		icon: string;
+		label: string;
+		open: boolean;
+	}
+
+	interface NavigationTopBarProps {
+		Navigation: NavigationProps[];
+	}
+	const { Navigation }: NavigationTopBarProps = $props();
+</script>
